b87722f04ab6a6be0f8519a643452d246125c28d
Gustavo Jun Yakushiji <gustavojun023@gmail.com>
2024-04-01 20:42:47 -0300

    C1

--- deploy.R
+++ deploy.R
@@ -3,3 +3,7 @@
 ## Fonte: https://medium.com/@rami.krispin/deploy-shiny-app-on-github-pages-b4cbd433bdc
 
 shinylive::export(appdir = "~/Documents/PPGEEA/24-1/Modelos-lineares/seminario/normal-multi", destdir = "docs")
+
+
+## Fonte:
+
