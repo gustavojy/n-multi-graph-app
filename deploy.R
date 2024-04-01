a8edee613168f961865d0507e1034450e5ca494f
Gustavo Jun Yakushiji <gustavojun023@gmail.com>
2024-04-01 20:59:16 -0300

    C3

--- deploy.R
+++
@@ -1,9 +0,0 @@
-# Deploy app shiny to github pages
-
-## Fonte: https://medium.com/@rami.krispin/deploy-shiny-app-on-github-pages-b4cbd433bdc
-
-shinylive::export(appdir = "~/Documents/PPGEEA/24-1/Modelos-lineares/seminario/normal-multi", destdir = "docs")
-
-
-## Fonte:
-
